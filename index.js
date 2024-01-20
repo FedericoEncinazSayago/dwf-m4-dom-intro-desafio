5e4c3202df1d3c6535544959c5c766d232ae1275
Federico Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2024-01-20 18:20:46 -0300

    Desafio termiando :)

--- index.js
+++ index.js
@@ -25,6 +25,30 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function createLiElWithCosasQueAprendimos(ulElement) {
+  cosasQueAprendimos.forEach((element) => {
+    const newLiEl = document.createElement("li");
+    const classItem = element.class || "item";
+    newLiEl.innerHTML = element.tema;
+
+    newLiEl.classList.add(classItem);
+    ulElement.appendChild(newLiEl);
+  });
+}
+
+function removeLiElements(ulElement) {
+  const liElements = ulElement.querySelectorAll("li");
+
+  for (const li of liElements) {
+    li.remove();
+  }
+}
+
+function main() {
+  const ulEl = document.querySelector(".lista");
+
+  removeLiElements(ulEl);
+  createLiElWithCosasQueAprendimos(ulEl);
+}
 
 main();
